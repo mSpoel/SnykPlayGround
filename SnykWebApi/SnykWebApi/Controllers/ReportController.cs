665831e3d1e45108a07542d7cbf68d5d40cb04f6
Mark van der Spoel <mark.vanderspoel@sunwebgroup.com>
2022-04-28 11:05:31 +0200

    Add reports controller

---
+++ SnykWebApi/SnykWebApi/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SnykWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        [HttpPost("download")]
+        public IActionResult DownloadReports([FromBody] string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return NotFound();
+            }
+
+            string dirName = Path.Combine(path, "Reports");
+
+            return Ok();
+        }
+
+    }
+}
